98fa6505b13f5cc3a04f3e7187c23a3c282ecba5
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-08-28 13:14:56 +0200

    Kiirás - print

---
+++ 03-print/kiiras.py
@@ -0,0 +1,15 @@
+print("Helló világ!")
+
+print("Szia!");print("Itt vagyok.")
+
+print("Egy");print();print("Kettő");
+
+
+print('alma', 'tök' , sep=' -=- ', end='\t<itt a vége>\n\n')
+
+
+print(1,2,3,4,5,sep=". ")
+
+print("Szia",end='!');
+
+print("Ma szép idő van.\nElmehetnénk az állatkertbe.")
